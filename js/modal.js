74e4b32aea739f4f06bf26ff97759e20befe7890
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-02-22 13:14:32 +0100

    added animation to modal

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    modalWindow: document.querySelector('[data-modal-window]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    refs.modalWindow.classList.toggle('animation-modal');
   }
 })();
